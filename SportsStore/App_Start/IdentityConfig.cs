76a63a511326960b14c8765d8e9b2a8d3381efc7
Duncan <duncan_311@yahoo.com>
2016-05-06 15:50:46 -0700

    adding repo

--- SportsStore/App_Start/IdentityConfig.cs
+++ SportsStore/App_Start/IdentityConfig.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.Owin;
+using Owin;
 
-namespace SportsStore.App_Start
+[assembly: OwinStartup(typeof(SportsStore.IdentityConfig))]
+
+namespace SportsStore
 {
     public class IdentityConfig
     {
-
+        public void Configuration(IAppBuilder app) { }
     }
 }
---
+++ SportsStore/Models/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SportsStore.Models
+{
+    public class ProductRepository : IRepository 
+    {
+        private ProductDbContext context = new ProductDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+
+        public async Task<int>
+    }
+}
